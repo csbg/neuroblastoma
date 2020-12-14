2ac32ee2e69884431ad11bb1ca92fef0937a6d03
Wolfgang Esser-Skala <wolfgang@esser-skala.at>
2020-12-14 11:17:26 +0100

    updates SingleR workflow to store all generated data

--- cell_types_singler.R
+++ cell_types_singler.R
@@ -1,33 +1,39 @@
 # Cell type assignment via SingleR for an integrated Seurat dataset
 #
-# Adds metadata on clustering to a Seurat object, as well as two columns
-# SingleR_cells and SingleR_clusters, containing predicted cell types on
-# the individual cell and cluster level, respectively.
+# Creates three results files:
+# (1) a CSV file with cell barcodes, tSNE/UMAP coordinates, and cell types
+# (2) a RDS file containing the dataframe returned by SingleR::SingleR()
+# (3) a CSV file containing all data in (2)
 
-library(magrittr)
+
 library(Seurat)
 library(SingleR)
 library(celldex)
+library(tidyverse)
 
 
 # small test datasets
-seurat_infile <- "data_generated/5_datasets/nb_integrated.rds"
-seurat_outfile <- "data_generated/5_datasets/nb_integrated_singler.rds"
+infile <- "data_generated/5_datasets/nb_integrated.rds"
+outfile <- "data_generated/5_datasets/singler_results"
 
 # large complete datasets
-# seurat_infile <- "data_generated/all_datasets/nb_integrated.rds
-# seurat_outfile <- "data_generated/all_datasets/nb_integrated_singler.rds"
+# infile <- "data_generated/all_datasets_sc/nb_integrated.rds"
+# outfile <- "data_generated/all_datasets_sc/singler_results"
+
+# whether to use broad or fine labels
+use_fine_labels <- TRUE
+# use_fine_labels <- FALSE
 
 
 
 # Load data ---------------------------------------------------------------
 
-nb <- readRDS(seurat_infile)
+nb <- readRDS(infile)
 
 nb <- 
   nb %>% 
   FindNeighbors() %>% 
-  FindClusters(resolution = .5)
+  FindClusters(resolution = 0.5)  # or 0.8 for more clusters
 
 # for testing: choose only a few cells
 # nb <- subset(nb, cells = sample(ncol(nb), 2000))
@@ -36,36 +42,35 @@ reference_cell_types <- HumanPrimaryCellAtlasData()
 
 
 
-# Predict types of individual cells ---------------------------------------
+# Predict cell types ------------------------------------------------------
 
 predicted_cell_types <- SingleR(
   GetAssayData(nb$RNA),
   reference_cell_types,
-  labels = reference_cell_types$label.main
+  labels = if (use_fine_labels)
+    reference_cell_types$label.fine
+  else
+    reference_cell_types$label.main
 )
-dim(predicted_cell_types)
 
 
-# Predict types of clusters -----------------------------------------------
 
-predicted_cluster_types <- SingleR(
-  GetAssayData(nb$RNA),
-  reference_cell_types,
-  labels = reference_cell_types$label.main,
-  clusters = Idents(nb)
-)
-predicted_cluster_types
+# Save data ---------------------------------------------------------------
 
+list(
+  nb@meta.data,
+  if ("tsne" %in% names(nb@reductions)) Embeddings(nb, "tsne"),
+  if ("umap" %in% names(nb@reductions)) Embeddings(nb, "umap")
+) %>% 
+  compact() %>% 
+  map(as_tibble, rownames = "cell") %>%
+  reduce(inner_join, by = "cell") %>%
+  mutate(SingleR_cell_types = predicted_cell_types$labels) %>%
+  write_csv(str_glue("{outfile}.csv"))
 
-# Combine data ------------------------------------------------------------
+predicted_cell_types %>% 
+  as_tibble(rownames = "cell") %>% 
+  write_csv(str_glue("{outfile}_details.csv"))
 
-nb <-
-  nb %>%
-  AddMetaData(predicted_cell_types$labels, "SingleR_cells") %>%
-  AddMetaData(predicted_cluster_types[Idents(nb),]$labels, "SingleR_clusters")
-
-# DimPlot(nb)
-# DimPlot(nb, group.by = "SingleR_cells")
-# DimPlot(nb, group.by = "SingleR_clusters")
-
-saveRDS(nb, seurat_outfile)
+predicted_cell_types %>% 
+  saveRDS(str_glue("{outfile}_details.rds"))
