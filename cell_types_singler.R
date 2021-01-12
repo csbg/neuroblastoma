8b381bf939d2c71597a3d36d7f589a03b1001ccd
Wolfgang Esser-Skala <wolfgang@esser-skala.at>
2021-01-12 12:58:07 +0100

    now generates a single output file with cell type information (used for v4 of the dataset)

--- cell_types_singler.R
+++ cell_types_singler.R
@@ -1,8 +1,8 @@
 # Cell type assignment via SingleR for an integrated Seurat dataset
 #
-# Creates three CSV files:
-# (1) nb_singler.csv – cell barcodes and unfiltered cell types
-# (2) nb_singler_details.csv – the dataframe returned by SingleR::SingleR()
+# Creates two CSV files:
+# (1) nb_singler.csv – the dataframe returned by SingleR::SingleR(), with added
+#                      cell and sample information
 # (3) nb_singler_degenes.csv – @metadata$de.genes of SingleR results (wide form)
 
 
@@ -10,15 +10,16 @@ library(Seurat)
 library(SingleR)
 library(celldex)
 library(tidyverse)
+library(fs)
 
 
 # small test datasets
 # infile <- "data_generated/3_datasets/nb_integrated.rds"
-# outfile <- "data_generated/3_datasets/nb_singler"
+# outdir <- "data_generated/3_datasets"
 # 
 # large complete datasets
 infile <- "data_generated/all_datasets_current/nb_integrated.rds"
-outfile <- "data_generated/all_datasets_current/nb_singler"
+outdir <- "data_generated/all_datasets_current"
 
 # whether to use broad or fine labels
 use_fine_labels <- TRUE
@@ -54,14 +55,8 @@ predicted_cell_types <- SingleR(
 nb@meta.data %>% 
   select(sample) %>% 
   as_tibble(rownames = "cell") %>% 
-  bind_cols(cell_type = predicted_cell_types$labels) %>% 
-  write_csv(str_glue("{outfile}.csv"))
-
-nb@meta.data %>% 
-  select(sample) %>% 
-  as_tibble(rownames = "cell") %>% 
   bind_cols(as_tibble(predicted_cell_types)) %>% 
-  write_csv(str_glue("{outfile}_details.csv"))
+  write_csv(path_join(c(outdir, "nb_singler.csv")))
 
 tibble(
   cell_type_1 = names(predicted_cell_types@metadata$de.genes),
@@ -71,5 +66,5 @@ tibble(
   rowwise() %>% 
   mutate(genes = str_c(genes, collapse = ", ")) %>% 
   relocate(cell_type_2, .after = cell_type_1) %>% 
-  write_csv(str_glue("{outfile}_degenes.csv"))
+  write_csv(path_join(c(outdir, "nb_singler_degenes.csv")))
 
