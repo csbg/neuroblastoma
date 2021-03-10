34f85f05926be896c2cd1bc9f05155ea34305545
Wolfgang Esser-Skala <wolfgang.esser-skala@sbg.ac.at>
2021-03-10 16:39:04 +0100

    improves (reimplements) feature plot of NB markers

--- plot_markers.R
+++ plot_markers.R
@@ -1,9 +1,9 @@
+# Plot canonical cell type and neuroblastoma markers.
+# Exports several plots to plots/markers.
+
 library(Seurat)
-library(SeuratObject)
 library(tidyverse)
-library(fs)
-library(patchwork)
-library(viridis)
+library(scico)
 source("common_functions.R")
 
 
@@ -11,10 +11,9 @@ source("common_functions.R")
 # Load data ---------------------------------------------------------------
 
 nb <- readRDS("data_generated/assay_SCT_seurat.rds")
-nb_data <- readRDS("data_generated/metadata.rds")
 
 nb@meta.data <-
-  nb_data %>%
+  readRDS("data_generated/metadata.rds") %>%
   column_to_rownames("cell")
 
 
@@ -23,8 +22,6 @@ nb@meta.data <-
 
 markers <- read_csv("metadata/cell_markers.csv", comment = "#")
 
-
-# monocle clusters
 Idents(nb) <- 
   fct_relevel(
     nb@meta.data$cluster_50,
@@ -58,60 +55,88 @@ Idents(nb) <-
 
 DotPlot(nb, features = rev(markers$gene)) +
   coord_flip() +
-  scale_color_viridis(option = "inferno", direction = -1)
-ggsave_default("monocle/markers/canonical_markers_clusters",
+  scale_color_scico(palette = "oslo", direction = -1)
+ggsave_default("markers/canonical_markers_clusters",
                height = 297, width = 210)
-ggsave_default("monocle/markers/canonical_markers_clusters",
+ggsave_default("markers/canonical_markers_clusters",
                type = "pdf", height = 297, width = 210, crop = FALSE)
 
 
 
 # NB markers --------------------------------------------------------------
 
+#' Normalize expression values.
+#' 
+#' Values are first limited to the given lower and upper quantile (which are
+#' calculated after exclusing zero values) and then normalized to the range [0,
+#' 1].
+#'
+#' @param x Numeric vector.
+#' @param lower_quantile Quantile for calculating the lower limit.
+#' @param upper_quantile Quantile for calculating the upper limit.
+#'
+#' @return The quantile-normalized numeric vector.
+normalize_expression <- function(x, lower_quantile, upper_quantile) {
+  q <- quantile(x[x > 0], c(lower_quantile, upper_quantile))
+  x_limited <- pmax(pmin(x, q[2]), q[1])
+  (x_limited - min(x_limited)) / (max(x_limited) - min(x_limited))
+}
+
+
+#' Color single cells on a dimensional reduction plot by normalized expression.
+#'
+#' @param data A Seurat object.
+#' @param x Column with x-axis data.
+#' @param y Column with y-axis data.
+#' @param features Genes to plot.
+#' @param lower_quantile Cut low expression values at this quantile.
+#' @param upper_quantile Cut high expression values at this quantile.
+#' @param filename Name of output file.
+#'
+#' @return A ggplot object.
+plot_features <- function(data, x, y, features,
+                          lower_quantile = 0.05, upper_quantile = 0.95,
+                          filename = NULL) {
+  expr_data <-
+    data$RNA@data[features, , drop = FALSE] %>%
+    as.matrix() %>% 
+    t() %>%
+    apply(
+      2,
+      normalize_expression,
+      lower_quantile = lower_quantile,
+      upper_quantile = upper_quantile
+    ) %>%
+    as_tibble()
+  
+  p <-
+    bind_cols(
+      data@meta.data %>%
+        as_tibble() %>%
+        select({{x}}, {{y}}),
+      expr_data
+    ) %>%
+    pivot_longer(!1:2, names_to = "feature", values_to = "expression") %>%
+    arrange(expression) %>%
+    ggplot(aes({{x}}, {{y}})) +
+    geom_point(aes(color = expression), size = 0.1) +
+    scale_color_scico("relative\nnormalized\nexpression", palette = "bamako") +
+    coord_fixed() +
+    facet_wrap(vars(feature)) +
+    theme_classic() +
+    theme(
+      strip.background = element_blank(),
+      strip.text = element_text(face = "bold")
+    )
+
+  ggsave_default(filename, width = 420, height = 297)
+  p
+}
+
 nb_markers <-
   markers %>%
   filter(cell_type == "neuroblastoma") %>%
   pull(gene)
 
-# add monocle UMAP coordinates to the Seurat object
-nb@reductions$umap_monocle <- CreateDimReducObject(
-  embeddings = nb_data %>% 
-    column_to_rownames("cell") %>% 
-    select(UMAP_1 = umap_1_monocle, UMAP_2 = umap_2_monocle) %>% 
-    as.matrix(),
-  assay = "monocle_align_cds",
-  global = TRUE,
-  key = "UMAPMON_"
-)
-
-FeaturePlot(
-  nb,
-  nb_markers,
-  reduction = "umap_monocle",
-  min.cutoff = "q5",
-  max.cutoff = "q95",
-  coord.fixed = TRUE,
-  ncol = 4,
-  order = TRUE
-) &
-  scale_color_viridis(option = "cividis")
-ggsave_default("monocle/markers/nb_markers", width = 420, height = 297)
-
-
-map(
-  levels(nb@meta.data$group),
-  function(g) {
-    message("Plotting dotplot for group ", g)
-    group_cells <-
-      nb@meta.data %>%
-      as_tibble(rownames = "cell") %>%
-      filter(group == g) %>%
-      pull(cell)
-    DotPlot(nb[, group_cells], features = nb_markers) +
-      coord_flip() +
-      scale_color_viridis(option = "inferno", direction = -1)
-  }
-) %>% 
-  wrap_plots()
-ggsave_default(str_glue("monocle/markers/nb_dotplot_groupwise"),
-               height = 200, width = 400)
+plot_features(nb, umap_1_monocle, umap_2_monocle, nb_markers,
+              filename = "markers/nb_markers_new")
