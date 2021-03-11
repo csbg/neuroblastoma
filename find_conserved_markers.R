b16debf60d1ee982427c506cc9713728fdfab876
Wolfgang Esser-Skala <wolfgang.esser-skala@sbg.ac.at>
2021-03-11 15:19:38 +0100

    uses decontaminated expression data for detecting conserved markers, improves visualization

--- find_conserved_markers.R
+++ find_conserved_markers.R
@@ -1,16 +1,16 @@
 # Find conserved markers.
 #
-# @DEPI rna_integrated_monocle.rds
+# @DEPI rna_decontaminated.rds
 # @DEPI metadata.rds
 
 
 library(monocle3)
 library(scran)
 library(scuttle)
-library(pheatmap)
+library(ComplexHeatmap)
 library(tidyverse)
-library(RColorBrewer)
 library(patchwork)
+library(scico)
 source("common_functions.R")
 
 
@@ -19,10 +19,10 @@ source("common_functions.R")
 # Load data ---------------------------------------------------------------
 
 nb <-
-  readRDS("data_generated/rna_integrated_monocle.rds") %>% 
+  readRDS("data_generated/rna_decontaminated.rds") %>% 
   logNormCounts()
 
-nb@colData <-
+colData(nb) <-
   readRDS("data_generated/metadata.rds") %>%
   mutate(Size_Factor = colData(nb)$Size_Factor) %>% 
   column_to_rownames("cell") %>% 
@@ -92,7 +92,7 @@ assemble_markers <- function(data,
 plot_conserved_markers <- function(data,
                                    markers,
                                    group,
-                                   dotplot_groups = "cluster",
+                                   dotplot_groups = "cluster_50",
                                    n_genes = 30,
                                    pval_max = 1e-6,
                                    pval_type = c("any", "some", "all"),
@@ -101,38 +101,100 @@ plot_conserved_markers <- function(data,
   sort_col <- "t.summary.logFC"
   prefix <- "t.logFC."
   
-  dge_df <- 
-    markers[[pval_type]][[group]] %>% 
+  clusters <- tribble(
+    ~id, ~type,
+    "3",  "T",
+    "5",  "T",
+    "18", "T",
+    "4",  "NK",
+    "6",  "NK",
+    "2",  "B",
+    "9",  "B",
+    "12", "B",
+    "17", "B",
+    "19", "B",
+    "21", "B",
+    "1",  "M",
+    "15", "M",
+    "16", "M",
+    "22", "M",
+    "14", "D",
+    "13", "E",
+    "7",  "other",
+    "10", "other",
+    "11", "other",
+    "20", "other",
+    "8",  "NB"
+  )
+  
+  colData(data)[[dotplot_groups]] <- 
+    colData(data)[[dotplot_groups]] %>% 
+    fct_relevel(clusters$id)
+
+  dge_df <-
+    markers[[pval_type]][[group]] %>%
     as_tibble(rownames = "gene") %>%
     filter(p.value < pval_max) %>%
     slice_head(n = n_genes) %>%
     arrange(desc(!!sym(sort_col)))
-  
-  dge_matrix <- 
-    dge_df %>% 
-    select(gene, starts_with(prefix)) %>% 
+
+  dge_matrix <-
+    dge_df %>%
+    select(gene, starts_with(prefix)) %>%
     rename_with(~str_replace(.x, prefix, "")) %>% 
+    mutate({{group}} := NA_real_) %>% 
     column_to_rownames("gene") %>%
-    as.matrix()
-  
-  break_max <- quantile(dge_matrix, 0.95)
+    as.matrix() %>% 
+    magrittr::extract(, clusters$id)
+
+  break_max <- quantile(dge_matrix, 0.95, na.rm = TRUE)
+
+  heatmap <- Heatmap(
+    dge_matrix,
+    col = circlize::colorRamp2(
+      seq(-break_max, break_max, length.out = 7),
+      rev(scico(n = 7, palette = "roma"))
+    ),
+    cluster_columns = FALSE,
+    cluster_rows = FALSE,
+    name = "expression",
+    top_annotation = HeatmapAnnotation(
+      "cell type" =
+        clusters %>%
+        filter(id %in% colnames(dge_matrix)) %>%
+        pull(type),
+      col = list("cell type" = c(
+        "T" = "#1f78b4",
+        NK = "#a6cee3",
+        B = "#33a02c",
+        M = "#ff7f00",
+        NB = "#e31a1c",
+        E = "#b15928",
+        other = "#6a3d9a",
+        D = "#fdbf6f"
+      ))
+    )
+  )
 
   p <- wrap_plots(
-    pheatmap(
-      dge_matrix,
-      color = colorRampPalette(rev(brewer.pal(n = 7, name = "RdBu")))(100),
-      breaks = seq(-break_max, break_max, length.out = 101),
-      cluster_cols = FALSE,
-      cluster_rows = FALSE,
-      silent = TRUE
-    )$gtable,
+    grid.grabExpr(draw(heatmap, merge_legend = TRUE)),
     plot_genes_by_group(
       data,
       rownames(dge_matrix) %>% rev(),
       group_cells_by = dotplot_groups,
       ordering_type = "none"
-    )
+    ) +
+      scale_color_scico(
+        name = "log(mean + 0.1)",
+        palette = "acton",
+        direction = -1
+      ) +
+      scale_radius(name = "percentage", range = c(0, 6)) +
+      xlab(NULL) +
+      ylab(NULL) +
+      theme(axis.text.x = element_text(angle = 0, hjust = 0.5))
   ) +
+    plot_layout(widths = c(1.5, 1)) +
     plot_annotation(str_glue("Group {group} in {dotplot_groups}"))
   ggsave_default(filename)
   p
@@ -142,49 +204,25 @@ plot_conserved_markers <- function(data,
 
 # Analysis ----------------------------------------------------------------
 
-## Clusters ----
+conmarkers_clusters <- assemble_markers(
+  nb,
+  colData(nb)$cluster_50,
+  pval_types = "any"
+)
 
-conmarkers_clusters <-
-  assemble_markers(nb, colData(nb)$cluster_50, pval_types = "any")
+plot_conserved_markers(nb, conmarkers_clusters, group = "8",
+                       filename = "markers/conserved_markers_c8")
 
-plot_conserved_markers(nb, conmarkers_clusters,
-                       group = "8", dotplot_groups = "cluster_50",
-                       filename = "monocle/markers/conserved_markers_c_8")
-
-levels(colData(nb)$cluster_50) %>% 
-  walk(
-    function(cluster) {
-      message("Plotting cluster ", cluster)
-      plot_conserved_markers(
-        nb,
-        conmarkers_clusters,
-        group = cluster,
-        dotplot_groups = "cluster_50",
-        filename = str_glue("monocle/markers/conserved_markers/c_{cluster}")
-      ) 
-    }
-  )
-
-
-## Partitions ----
-
-conmarkers_partitions <-
-  assemble_markers(nb, colData(nb)$partition_50, pval_types = "any")
-
-plot_conserved_markers(nb, conmarkers_partitions,
-                       group = "1", dotplot_groups = "partition_50",
-                       filename = "monocle/markers/conserved_markers_p_1")
-
-levels(colData(nb)$partition_50) %>% 
-  walk(
-    function(partition) {
-      message("Plotting partition ", partition)
-      plot_conserved_markers(
-        nb,
-        conmarkers_partitions,
-        group = partition,
-        dotplot_groups = "partition_50",
-        filename = str_glue("monocle/markers/conserved_markers/p_{partition}")
-      ) 
-    }
-  )
+walk(
+  levels(colData(nb)$cluster_50),
+  function(cluster) {
+    message("Plotting cluster ", cluster)
+    plot_conserved_markers(
+      nb,
+      conmarkers_clusters,
+      group = cluster,
+      dotplot_groups = "cluster_50",
+      filename = str_glue("markers/conserved_markers_c{cluster}")
+    ) 
+  }
+)
