86e5ab0c88bfbce513bc6ccee813c6f60d4dd0ae
Wolfgang Esser-Skala <wolfgang.esser-skala@plus.ac.at>
2021-10-12 08:47:18 +0200

    removes unused analyses

--- rna_velocity/run_scvelo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-PROJECTDIR="/usr/local/AGFORTELNY/PROJECTS/Neuroblastoma"
-PROJECTDIR_SIF="/media/AGFORTELNY"
-
-singularity run \
-  --bind $PROJECTDIR:$PROJECTDIR_SIF \
-  scvelo.sif \
-  python analyse_velocity.py
